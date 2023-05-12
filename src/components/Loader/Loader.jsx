e3f45e474ebda7230d08431341955f7598afd274
Taras Kachmar <kachmar.taras@gmail.com>
2023-05-12 03:23:55 +0300

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,25 +1,24 @@
-import { Component } from 'react';
 import { createPortal } from 'react-dom';
 import { Vortex } from 'react-loader-spinner';
 import { LoaderWrap } from 'components/Loader/Loader.styled';
 
 const rootloader = document.querySelector('#rootLoader');
 
-export default class Loader extends Component {
-  render() {
-    return createPortal(
-      <LoaderWrap>
-        <Vortex
-          type="watch"
-          visible={true}
-          height="100"
-          width="100"
-          ariaLabel="vortex-loading"
-          wrapperStyle={{}}
-          colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
-        />
-      </LoaderWrap>,
-      rootloader
-    );
-  }
-}
+const Loader = () => {
+  return createPortal(
+    <LoaderWrap>
+      <Vortex
+        type="watch"
+        visible={true}
+        height="100"
+        width="100"
+        ariaLabel="vortex-loading"
+        wrapperStyle={{}}
+        colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
+      />
+    </LoaderWrap>,
+    rootloader
+  );
+};
+
+export default Loader;
